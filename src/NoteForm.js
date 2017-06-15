e10f70958c240846575bcb43863bca61fc7c8f9d
Sari Stissi <sestissi16@earlham.edu>
2017-06-14 22:24:31 -0400

    Finish adding the basic components from index.html and their corresponding css

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -4,7 +4,16 @@ import './NoteForm.css'
 class NoteForm extends Component{
     render(){
         return(
-            <p>NoteForm!</p>
+            <div className="NoteForm">
+                <form>
+                    <p>
+                        <input type="text" name="title" placeholder="Title your note" value="" />
+                    </p>
+                    <p>
+                        <textarea name="body" cols="30" rows="10" placeholder="Just start typing..."></textarea>
+                    </p>
+                </form>
+            </div>
         )
     }
 }
