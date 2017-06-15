9163ad8a8fe863f942503a6b92a0ac0ac4d909ad
Sari Stissi <sestissi16@earlham.edu>
2017-06-15 10:49:30 -0400

    Add working delete button

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,16 +2,70 @@ import React, { Component } from 'react'
 import './NoteForm.css'
 
 class NoteForm extends Component{
+    constructor(){
+        super()
+        this.state = {
+            title: '',
+            body: '',
+        }
+        this.saveNote = this.saveNote.bind(this)
+        this.updateNoteTitle = this.updateNoteTitle.bind(this)
+        this.updateNoteBody = this.updateNoteBody.bind(this)
+    }
+    updateNoteTitle(ev){
+        this.setState({
+            title: ev.target.value
+        })
+
+    }
+    updateNoteBody(ev){
+        this.setState({
+            body: ev.target.value
+        })
+    }
+    saveNote(ev){
+        ev.preventDefault()
+        return(
+            // console.log(this.state)
+            <li>
+                <div className="note">
+                    <div className="note-title">
+                        {this.state.title}
+                    </div>
+                    <div className="note-body">
+                        <p>
+                            {this.state.body}
+                        </p>
+                    </div>
+                </div>
+            </li>
+        )
+    }
+
     render(){
         return(
             <div className="NoteForm">
                 <form>
                     <p>
-                        <input type="text" name="title" placeholder="Title your note" value="" />
+                        <input 
+                            type="text" 
+                            name="title" 
+                            placeholder="Title your note" 
+                            value={this.state.title}
+                            onChange={this.updateNoteTitle}
+                        />
                     </p>
                     <p>
-                        <textarea name="body" cols="30" rows="10" placeholder="Just start typing..."></textarea>
+                        <textarea 
+                            name="body" 
+                            cols="30" 
+                            rows="10" 
+                            placeholder="Just start typing..."
+                            value={this.state.body}
+                            onChange={this.updateNoteBody}
+                        ></textarea>
                     </p>
+                    <button onClick={this.saveNote} className="button">Save form</button>
                 </form>
             </div>
         )
--- src/NoteList.js
+++ src/NoteList.js
@@ -1,48 +1,79 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './NoteList.css'
 
-const NoteList = () =>{
-    return(
-        <div className="NoteList">
-            <h3>Notes</h3>
-            <ul id="notes">
-                <li>
-                    <div className="note">
-                        <div className="note-title">
-                            Citizens of distant epochs
-                        </div>
-                        <div className="note-body">
-                            <p>
-                                Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?
-                            </p>
-                        </div>
-                    </div>
-                </li>
+class NoteList extends Component{
+    deleteNote(ev){
+        ev.preventDefault()
+        ev.target.parentElement.parentElement.remove()
 
-                <li>
-                    <div className="note">
-                        <div className="note-title">Preserve and cherish that pale blue dot </div>
-                        <div className="note-body">
-                            <p>
-                                network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!
-                            </p>
+    }
+    render(){
+        return(
+            <div className="NoteList">
+                <h3>Notes</h3>
+                <ul id="notes">
+                    <li>
+                        <div className="note">
+                            <div className="note-title">
+                                Citizens of distant epochs
+                            </div>
+                            <div className="note-body">
+                                <p>
+                                    Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?
+                                </p>
+                            </div>
+                            <button className="warning button" onClick={this.deleteNote.bind(this)}>
+                                {/*<i 
+                                    className="fa-trash-o"
+                                    aria-hidden="true"
+                                >
+                                </i>*/}
+                                🗑️
+                            </button>
                         </div>
-                    </div>
-                </li>
+                    </li>
 
-                <li>
-                    <div className="note">
-                        <div className="note-title">Laws of physics</div>
-                        <div className="note-body">
-                            <p>
-                                Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas
-                            </p>
+                    <li>
+                        <div className="note">
+                            <div className="note-title">Preserve and cherish that pale blue dot </div>
+                            <div className="note-body">
+                                <p>
+                                    network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!
+                                </p>
+                            </div>
+                            <button className="warning button" onClick={this.deleteNote.bind(this)}>
+                                {/*<i 
+                                    className="fa-trash-o"
+                                    aria-hidden="true"
+                                >
+                                </i>*/}
+                                🗑️
+                            </button>
                         </div>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    )
+                    </li>
+
+                    <li>
+                        <div className="note">
+                            <div className="note-title">Laws of physics</div>
+                            <div className="note-body">
+                                <p>
+                                    Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas
+                                </p>
+                            </div>
+                            <button className="warning button" onClick={this.deleteNote.bind(this)}>
+                                {/*<i 
+                                    className="fa-trash-o"
+                                    aria-hidden="true"
+                                >
+                                </i>*/}
+                                🗑️
+                            </button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default NoteList
