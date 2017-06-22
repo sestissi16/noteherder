7b8fa6c2e2e0a307f686eea58482dd2048e9492a
Sari Stissi <sestissi16@earlham.edu>
2017-06-22 04:32:51 -0400

    Style background and add welcome title and slogan to login page

--- src/SignIn.js
+++ src/SignIn.js
@@ -9,25 +9,29 @@ const SignIn = () => {
   }
 
   return (
-    <div>
-      <button
-        className="SignIn"
-        onClick={() => authenticate(githubProvider)}
-      >
-        Sign in with GitHub
-      </button>
-      <button
-        className="SignIn"
-        onClick={() => authenticate(googleProvider)}
-      >
-        Sign in with Google
-      </button>
-      <button
-        className="SignIn"
-        onClick={() => authenticate(facebookProvider)}
-      >
-        Sign in with Facebook
-      </button>
+    <div className="div-container">
+      <div className="SignInPage">
+        <h1 className="welcome">Welcome to <span>Noteherder</span></h1>
+        <h3 className="solgan"><span>Where we round up all of those pesky notes!</span></h3>
+        <button
+          className="SignIn"
+          onClick={() => authenticate(githubProvider)}
+        >
+          Sign in with GitHub
+        </button>
+        <button
+          className="SignIn"
+          onClick={() => authenticate(googleProvider)}
+        >
+          Sign in with Google
+        </button>
+        <button
+          className="SignIn"
+          onClick={() => authenticate(facebookProvider)}
+        >
+          Sign in with Facebook
+        </button>
+      </div>
     </div>
   )
 }
