e276bff090f5b5967e0dd321579c46367f7aa7d7
Sari Stissi <sestissi16@earlham.edu>
2017-07-26 19:13:58 +0200

    Style the sign in page with new background

--- src/SignIn.js
+++ src/SignIn.js
@@ -11,8 +11,9 @@ const SignIn = () => {
   return (
     <div className="div-container">
       <div className="SignInPage">
-        <h1 className="welcome">Welcome to <span>Noteherder</span></h1>
-        <h3 className="solgan"><span>Where we round up all of those pesky notes!</span></h3>
+        <h1 className="welcome">Welcome to </h1>
+        <h1 className="welcome-title"><span>Noteherder</span></h1>
+        {/* <h3 className="solgan"><span>Where we round up all of those pesky notes!</span></h3> */}
         <div className="button-container">
           <button
             className="SignInGithub"
